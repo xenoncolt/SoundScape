5da25420b258968d42ecd59da319e5addc6b9450
Xenon Colt <s.h.shahriar.haque@gmail.com>
2025-09-08 05:28:57 +0600

    redirect handler

--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
 <?php
-echo "<p>Hello, World! people wow</p>";
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$uri = $_SERVER['REQUEST_URI'];
+$path = parse_url($uri, PHP_URL_PATH);
+
+switch ($path) {
+    case '/':
+        include __DIR__ . '/pages/home.php';
+        break;    
+    default:
+        http_response_code(404);
+        echo "<h1>You r trying to access a page which doesn't exist</h1>";
+}
 ?>
